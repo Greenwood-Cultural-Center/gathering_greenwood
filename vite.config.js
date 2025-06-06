9f6ff989c8541037d6343370533efacc3d1dfb33
Keith Lewis <kclewis0614@gmail.com>
2025-06-06 12:29:14 -0500

    finalize build process

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import vue from '@vitejs/plugin-vue';
+import AutoZip from 'vite-plugin-auto-zip';
+
+const __APP_VERSION__ = process.env.npm_package_version;
+const __APP_NAME__ = process.env.npm_package_name;
+const __APP_DESCRIPTION__ = process.env.npm_package_description;
+
+const zipName = `${__APP_NAME__}-${__APP_VERSION__}.zip`;
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +15,8 @@ export default defineConfig({
     vue(),
     vueDevTools({
       launchEditor: 'code',
-    })
+    }),
+    AutoZip(zipName,'dist'),
   ],
   build: {
     cacheDir: '.vite', // Make sure the cache is stored in a persistent location
